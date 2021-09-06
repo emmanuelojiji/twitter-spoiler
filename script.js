8dc0ea42a9ae54f855a5fdcf550f45d568b5c079
Emmanuel <85620559+emmanuelojiji@users.noreply.github.com>
2021-09-06 18:00:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,15 +8,30 @@ window.onload = () => {
     setTimeout(() => {
       popup.style.maxHeight = "auto";
     }, 1000);
-
   };
 
-  window.onclick = (event) => {
-    if (event.target !== popup) {
-        console.log("outside box")
-    }
-  }
+  const leftArrow = document.querySelector(".left-arrow");
+  const writeTweetContainer = document.querySelector(".write-tweet-container");
+  const newTweetButton = document.querySelector(".new-tweet-container");
 
+  newTweetButton.onclick = () => {
+    writeTweetContainer.style.display = "block";
+  };
 
- 
+  leftArrow.onclick = () => {
+    writeTweetContainer.style.display = "none";
+  };
+
+  const tweetTextArea = document.querySelector(".tweet-text-area");
+  const sendTweetButton = document.querySelector(".send-tweet");
+
+  tweetTextArea.onkeydown = () => {
+    if (tweetTextArea.value.length >= -1) {
+      sendTweetButton.style.opacity = "1";
+      console.log("greater than 0");
+     } else if (tweetTextArea.value.length <= 0) {
+      sendTweetButton.style.opacity = "0.5";
+      console.log("0 or less");
+     }
+  };
 };
