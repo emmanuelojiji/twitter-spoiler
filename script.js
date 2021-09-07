05275eeaebae48f7604d9f2ea86ff7dde2364e2e
Emmanuel <85620559+emmanuelojiji@users.noreply.github.com>
2021-09-07 23:51:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-  const dots = document.querySelector(".dots-popup-container");
+  const dots = document.querySelector(".dot-wrap");
   const popup = document.querySelector(".popup");
 
   dots.onclick = () => {
