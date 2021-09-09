1b884ad834ce32dd83f7c9411b4fe019386ef397
Emmanuel <85620559+emmanuelojiji@users.noreply.github.com>
2021-09-09 20:08:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
 window.onload = () => {
+
+  const revealedTweet = document.querySelector('.revealed-tweet-container');
+  const warningContainer = document.querySelector('.warning-container');
+  const view = document.querySelectorAll('.view');
+
+
+  for (let index = 0; index < view.length; index++) {
+    view[index].onclick = () => {
+      warningContainer.style.display = 'none';
+      flaggedMessage.style.display = 'none';
+      revealedTweet.style.display = "block";
+      setTimeout(() => {
+        revealedTweet.style.opacity = '1';
+      },0);
+    }
+  }
+  
+
   const dots = document.querySelector(".dot-wrap");
   const popup = document.querySelector(".popup");
 
-  dots.onclick = () => {
+  
+  dots.onclick = (e) => {
     popup.style.display = "block";
+    e.stopPropagation();
+  }
 
-   /* setTimeout(() => {
-      popup.style.maxHeight = "auto";
-    }, 1000); */
+  document.onclick = (event) => {
+      let isClickInside = popup.contains(event.target);
+
+      if (!isClickInside) {
+        console.log("outside box");
+        popup.style.display = 'none';
+      }
+    
+
   };
 
-  const leftArrow = document.querySelector(".left-arrow");
+  const spoiler = document.querySelector(".spoiler");
+  const flaggedMessage = document.querySelector(".flagged-as-container");
+
+  spoiler.onclick = () => {
+    flaggedMessage.style.display = "block";
+    warningContainer.style.display = "none"
+    popup.style.display = "none";
+    revealedTweet.style.display = "none";
+  }
+
+  const cancel = document.querySelector(".cancel");
   const writeTweetContainer = document.querySelector(".write-tweet-container");
   const newTweetButton = document.querySelector(".new-tweet-container");
 
   newTweetButton.onclick = () => {
     writeTweetContainer.style.display = "block";
     tweetTextArea.focus();
+    document.documentElement.style.overflow = "hidden";
+    writeTweetContainer.style.overflow = "hidden";
+
   };
 
-  leftArrow.onclick = () => {
+  cancel.onclick = () => {
     writeTweetContainer.style.display = "none";
+    document.documentElement.style.overflow = "auto";
   };
 
   const tweetTextArea = document.querySelector(".tweet-text-area");
@@ -54,4 +95,12 @@ window.onload = () => {
       retweet.classList.add("is-retweeted");
     }
   };
+
+
+
+
+
+
+
+
 };
